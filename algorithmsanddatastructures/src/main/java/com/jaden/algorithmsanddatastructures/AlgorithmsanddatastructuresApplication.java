1c8e7bc7b14672dc815e96c1e8fc0e3ae1ef9eb0
jadenzhang <jadenzhangwy@163.com>
2020-10-12 10:59:37 +0800

    tijiaoceshi

---
+++ algorithmsanddatastructures/src/main/java/com/jaden/algorithmsanddatastructures/AlgorithmsanddatastructuresApplication.java
@@ -0,0 +1,13 @@
+package com.jaden.algorithmsanddatastructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsanddatastructuresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsanddatastructuresApplication.class, args);
+    }
+
+}
---
+++ algorithmsanddatastructures/src/test/java/com/jaden/algorithmsanddatastructures/AlgorithmsanddatastructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaden.algorithmsanddatastructures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsanddatastructuresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

2557c147f5bcbfbb2f2d8d5d2aaa26a483a62628
jadenzhangDong <846379354@qq.com>
2023-07-13 11:34:42 +0800

    提交新建工程测试

---
+++ java-core/src/TestGit.java
@@ -0,0 +1,7 @@
+import java.text.SimpleDateFormat;
+
+public class TestGit {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD");
+    }
+}
